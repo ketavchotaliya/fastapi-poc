e15edc1b349afe023b598f8395f9e42297b98ab9
ketav-mobiquity <ketavc@mobiquityinc.com>
2025-11-06 20:52:59 +0530

    Added CRUD operations with Blog APIs

--- repository/blog.py
+++ repository/blog.py
@@ -11,3 +11,44 @@ def create(request_body: BlogRequest, db: Session):
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+
+def get_all(db: Session):
+    blogs = db.query(Blogs).all()
+    return blogs
+
+
+def get(id: int, db: Session):
+    blog = db.query(Blogs).filter(Blogs.id == id).first()
+    if not blog:
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"success": False, "message": "Blog not found."}
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail={"success": False, "message": "Blog not found."})
+
+    return blog
+
+
+def delete(id: int, db: Session):
+    blog = db.query(Blogs).filter(Blogs.id == id).first()
+
+    if not blog:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail={"success": False, "message": "Blog not found."})
+    db.delete(blog)
+    db.commit()
+    return {"details": blog, "message": "Blog has been deleted."}
+
+
+def update(id: int, request_body: BlogRequest, db: Session):
+    blog = db.query(Blogs).filter(Blogs.id == id).first()
+
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail={
+                            "success": False, "message": "Blog not found."})
+
+    for key, value in request_body.dict().items():
+        setattr(blog, key, value)
+    db.commit()
+    db.refresh(blog)
+    return {"details": blog, "message": "Blog has been updated successfully."}
