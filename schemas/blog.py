964b50eb0bac8cf353f73a69b0160f99e10a2e98
ketav-mobiquity <ketavc@mobiquityinc.com>
2025-11-06 19:37:41 +0530

    Added pydentic schema for Blog

---
+++ schemas/blog.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+
+
+class BlogBase(BaseModel):
+    title: str
+    body: str
+
+
+class BlogRequest(BlogBase):
+    user_id: int
+
+
+class BlogResponse(BlogBase):
+    # creator: UserBase
+
+    class Config():
+        from_attributes = True
