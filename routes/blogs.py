cd67156e5d60b93d64176a79c784515c02a406a4
ketav-mobiquity <ketavc@mobiquityinc.com>
2025-11-08 17:35:33 +0530

    Make all blogs API a private API and requires valid auth token to access blogs APIs

--- routes/blogs.py
+++ routes/blogs.py
@@ -6,40 +6,49 @@ from schemas.blog import BlogResponse, BlogRequest
 from schemas.auth import TokenData
 from repository import blog as blog_repository
 
-router = APIRouter(
-    tags=["Blogs"],
-    prefix="/blogs"
-)
+router = APIRouter(tags=["Blogs"], prefix="/blogs")
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=BlogResponse)
 def create_blog(
     request_body: BlogRequest,
-    validate_auth: TokenData = Depends(verify_access_token), # ToDo validate this.. Token is missing
-    db: Session = Depends(database.get_db)
+    validate_auth: TokenData = Depends(verify_access_token),
+    db: Session = Depends(database.get_db),
 ):
     return blog_repository.create(request_body, db)
 
 
 @router.get("/", response_model=list[BlogResponse])
-def get_blogs(db: Session = Depends(database.get_db)):
+def get_blogs(
+    db: Session = Depends(database.get_db),
+    validate_auth: TokenData = Depends(verify_access_token),
+):
     return blog_repository.get_all(db)
 
 
 @router.get("/{id}", response_model=BlogResponse, status_code=status.HTTP_200_OK)
 def get_blog(
     id: int,
-    # validate_auth: schemas.TokenData = Depends(auth.verify_access_token),
-    db: Session = Depends(database.get_db)
+    validate_auth: str = Depends(verify_access_token),
+    db: Session = Depends(database.get_db),
 ):
     return blog_repository.get(id, db)
 
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_blog(id: int, db: Session = Depends(database.get_db)):
+def delete_blog(
+    id: int,
+    db: Session = Depends(database.get_db),
+    validate_auth: TokenData = Depends(verify_access_token),
+):
     return blog_repository.delete(id, db)
 
 
 @router.put("/{id}", status_code=status.HTTP_202_ACCEPTED)
-def update_blog(id: int, updated_blog: BlogRequest, db: Session = Depends(database.get_db)):
+def update_blog(
+    id: int,
+    updated_blog: BlogRequest,
+    db: Session = Depends(database.get_db),
+    validate_auth: TokenData = Depends(verify_access_token),
+):
     return blog_repository.update(id, updated_blog, db)
